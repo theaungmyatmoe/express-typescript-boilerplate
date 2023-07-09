6e12d6783c0c75a776fe4683ec0281d8795be896
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-10 02:52:56 +0630

    fix: unused vars

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,23 +2,22 @@ import { Request, Response } from 'express';
 import { UsersService } from './users.service';
 
 export class UsersController {
-
   constructor(
     private readonly usersService: UsersService = new UsersService(),
-  ) {
-  }
+  ) {}
 
   index(req: Request, res: Response) {
     return res.json({ message: 'User List' });
   }
 
   store(req: Request, res: Response) {
-
-    return this.usersService.create({
-      name: 'John',
-      email: 'john@test.com',
-      password: '123',
-    });
+    return res.json(
+      this.usersService.create({
+        name: 'John',
+        email: 'john@test.com',
+        password: '123',
+      }),
+    );
   }
 
   show(req: Request, res: Response) {
