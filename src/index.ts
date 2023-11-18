96552ef672b6cde6581796db6013251237ea3ab6
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-11-19 01:41:18 +0630

    feat: add docker

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import app from './app';
+import 'dotenv/config';
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
+});
+
+process.on('SIGINT', () => {
+  process.exit();
 });
